ae13f71d47cad2038de7de52d50a46310c04a20c
leonardsoncc <leonardson.carvalho@gmail.com>
2021-01-17 22:51:03 -0300

    nothing special

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,13 @@ killall -q polybar
 # Espere até que os processos em execução sejam terminados
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# execute a Polybar, usando a configuração padrão ~/.config/polybar/config
-polybar example &
+# Abrir polybar em todos os monitores
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar lançada..."
