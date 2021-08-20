bacdf86efe74e61ad643936399577d193f71af2f
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-08-20 16:26:54 -0300

    NvChad is great

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,12 @@
-require("leo.sets")
-require("leo.plugins")
-require("leo.keybindings")
-require("leo.lsp")
-require("leo.telescopeson")
-require("leo.galaxyline")
-require("leo.compe")
-require("leo.dashboard")
+local chad_modules = {
+   "options",
+   "mappings",
+}
 
--- need to improve this
-require("leo.projects")
+for i = 1, #chad_modules, 1 do
+   if not pcall(require, chad_modules[i]) then
+      error("Error loading " .. chad_modules[i] .. "\n")
+   end
+end
 
-vim.api.nvim_command("set termguicolors")
-vim.g.material_style = "deep ocean"
-require('material').set()
+require("mappings").misc()
--- .config/nvim/lua/leo/nvim-tree.lua
+++
@@ -1,6 +0,0 @@
--- Keybindings
-vim.api.nvim_set_keymap("n", "<Leader>fm", ":NvimTreeToggle<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>fc", ":NvimTreeFindFile<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>fr", ":NvimTreeRefresh<CR>", { noremap = true })
-
-vim.api.nvim_command("let g:nvim_tree_side = 'right'")
