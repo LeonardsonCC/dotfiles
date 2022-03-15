1b1d7b12ba9f07740e14ddb80f2d40e450ae504f
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-03-15 08:28:37 -0300

    starting to configure my fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     
-    fish_add_path -ag ~/.tfenv/bin
+    # PATH variables
+    fish_add_path -ag ~/dev/test
 end
