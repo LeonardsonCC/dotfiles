58d3172e47ba71048248442acd3e5085cafef57e
LeonardsonCC <leonardson.carvalho@catho.com>
2022-03-15 09:43:35 -0300

    fix fish config in Ubuntu

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     
     # PATH variables
-    fish_add_path -ag ~/dev/test
+    # fish_add_path -ag ~/dev/test
 
     # Source private config file if exists
     if not test -e ~/.env.fish
