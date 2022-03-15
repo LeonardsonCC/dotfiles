b2aad6ab3c2689248ee24d0c7f94b941d430ecaa
LeonardsonCC <leonardson.carvalho@catho.com>
2022-03-15 13:53:08 -0300

    nvim: terraform lsp

--- nvim/.config/nvim/lua/leo/lsp.lua
+++ nvim/.config/nvim/lua/leo/lsp.lua
@@ -13,6 +13,12 @@ vim.api.nvim_set_keymap("i", "<C-space>", "compe#complete()", { expr=true, norem
 vim.api.nvim_set_keymap("n", "<Leader>ct", ":TroubleToggle<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>cf", ":lua vim.lsp.buf.formatting()<CR>", { noremap = true })
 
+
+local on_attach = function (client)
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
+
 local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
@@ -21,17 +27,12 @@ null_ls.setup({
     null_ls.builtins.formatting.prettier,
   },
   on_attach = function(client)
+    on_attach(client)
     if client.resolved_capabilities.document_formatting then
       vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     end
   end,
 })
-
-
-local on_attach = function (client)
-  client.resolved_capabilities.document_formatting = false
-  client.resolved_capabilities.document_range_formatting = false
-end
 
 local lsp = require('lspconfig')
 lsp.tsserver.setup{
@@ -48,6 +49,12 @@ lsp.gopls.setup{
 			staticcheck = true,
 		},
 	},
+}
+lsp.terraformls.setup{
+  cmd = { "/home/leonardson/.local/share/nvim/lsp_servers/terraform/terraform-ls/terraform-ls", "serve" },
+  on_attach = function(client)
+    vim.cmd("autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()")
+  end,
 }
 
 vim.api.nvim_command("let g:neoformat_enabled_javascript = ['prettier']")
