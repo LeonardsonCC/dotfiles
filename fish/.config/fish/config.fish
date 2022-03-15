e2f16150d1578b888dddb18a75dc216cbe31bc3d
LeonardsonCC <leonardson.carvalho@catho.com>
2022-03-15 10:17:51 -0300

    trying to configure fish shell for work

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     
     # PATH variables
-    # fish_add_path -ag ~/dev/test
+    # fish_add_path -ag 
 
     # Source private config file if exists
     if not test -e ~/.env.fish
       touch ~/.env.fish
     end
     source ~/.env.fish
+
+    # NVM
+    bass source ~/.nvm/nvm.sh
 end
