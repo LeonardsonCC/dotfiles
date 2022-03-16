e1d61916b84e1888ee2206873494632d53a6c8dd
LeonardsonCC <leonardson.carvalho@catho.com>
2022-03-16 17:57:06 -0300

    wezterm: nice terminal

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm';
 return {
   default_prog = {"fish"},
+
   font = wezterm.font("mononoki", {weight="Regular"}),
+  font_size = 11,
+
   color_scheme = "Gruvbox Dark",
+  window_background_opacity = 0.9,
 }
