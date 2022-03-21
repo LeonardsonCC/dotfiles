86e938c0ce93f9a6a4850ef6c97c2c19ad4e27fa
LeonardsonCC <leonardson.carvalho@catho.com>
2022-03-21 10:37:42 -0300

    nvim: offset because nvimtree

--- nvim/.config/nvim/lua/leo/bufferline.lua
+++ nvim/.config/nvim/lua/leo/bufferline.lua
@@ -2,5 +2,6 @@ require("bufferline").setup {
   options = {
     diagnostics = "nvim_lsp",
     diagnostics_update_in_insert = true,
+    offsets = {{filetype = "NvimTree", text = "File Explorer"}},
   }
 }
