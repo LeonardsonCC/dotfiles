930f201cc6b7b98cdb1089bbdea6c1ec20c3019f
LeonardsonCC <leonardson.carvalho@catho.com>
2022-04-18 09:44:53 -0300

    neovim: updated and using astrovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,43 @@
-require('leo.plugins')
-require('leo.sets')
-require('leo.keybindings')
-require('leo.statusline')
-require('leo.lsp')
-require('leo.telescope')
-require('leo.cmp')
-require('leo.dashboard')
-require('leo.gitsigns')
-require('leo.bufferline')
+vim.opt.rtp:append(vim.fn.stdpath "config" .. "/../astronvim")
 
-vim.g.gruvbox_baby_transparent_mode = 1
-vim.cmd('colorscheme gruvbox-baby')
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then
+  impatient.enable_profile()
+end
+
+local utils = require "core.utils"
+
+utils.disabled_builtins()
+
+utils.bootstrap()
+
+local sources = {
+  "core.options",
+  "core.plugins",
+  "core.autocmds",
+  "core.mappings",
+  "configs.which-key-register",
+}
+
+for _, source in ipairs(sources) do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then
+    error("Failed to load " .. source .. "\n\n" .. fault)
+  elseif source == "core.plugins" then
+    utils.compiled()
+  end
+end
+
+local status_ok, ui = pcall(require, "core.ui")
+if status_ok then
+  for ui_addition, enabled in pairs(utils.user_settings().ui) do
+    if enabled and type(ui[ui_addition]) == "function" then
+      ui[ui_addition]()
+    end
+  end
+end
+
+local polish = utils.user_plugin_opts "polish"
+if type(polish) == "function" then
+  polish()
+end
