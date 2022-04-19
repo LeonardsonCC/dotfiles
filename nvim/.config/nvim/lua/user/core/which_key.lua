ce0437b3a062bba18b2769c6710adba9f2b6f6f4
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-04-18 21:57:34 -0300

    nvim: configuring astrovim for me

---
+++ nvim/.config/nvim/lua/user/core/which_key.lua
@@ -0,0 +1,6 @@
+return {
+    -- Add bindings to the normal mode <leader> mappings
+    register_n_leader = {
+      -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+    },
+}
---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,20 @@
+return {
+    -- Add plugins, the packer syntax without the "use"
+    init = {
+      -- { "andweeb/presence.nvim" },
+      -- {
+      --   "ray-x/lsp_signature.nvim",
+      --   event = "BufRead",
+      --   config = function()
+      --     require("lsp_signature").setup()
+      --   end,
+      -- },
+    },
+    -- All other entries override the setup() call for default plugins
+    treesitter = {
+      ensure_installed = { "lua" },
+    },
+    packer = {
+      compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
+    },
+}
