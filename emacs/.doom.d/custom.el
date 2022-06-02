350763673a37c69c19b7be33e26b846820f6b2f7
LeonardsonCC <leonardson.carvalho@catho.com>
2022-06-02 15:19:03 -0300

    doom emacs

---
+++ emacs/.doom.d/custom.el
@@ -0,0 +1,51 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d2b55" "#ff000d" "#3ad900" "#fad000" "#82aaff" "#ff9d00" "#ff628c" "#e3e9fa"])
+ '(custom-safe-themes
+   '("c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "c5ded9320a346146bbc2ead692f0c63be512747963257f18cc8518c5254b7bf5" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" default))
+ '(exwm-floating-border-color "#1e1e3f")
+ '(fci-rule-color "#444a73")
+ '(highlight-tail-colors
+   ((("#2e3c4c" "#2e3c4c" "green")
+     . 0)
+    (("#42305a" "#42305a" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#161a2a" "#82aaff"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#161a2a" "#3ad900"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#161a2a" "#444a73"))
+ '(objed-cursor-color "#ff000d")
+ '(pdf-view-midnight-colors (cons "#e3e9fa" "#2d2b55"))
+ '(rustic-ansi-faces
+   ["#2d2b55" "#ff000d" "#3ad900" "#fad000" "#82aaff" "#ff9d00" "#ff628c" "#e3e9fa"])
+ '(vc-annotate-background "#2d2b55")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#3ad900")
+    (cons 40 "#79d600")
+    (cons 60 "#b9d300")
+    (cons 80 "#fad000")
+    (cons 100 "#fbbf00")
+    (cons 120 "#fdae00")
+    (cons 140 "#ff9d00")
+    (cons 160 "#ff9d00")
+    (cons 180 "#ff9d00")
+    (cons 200 "#ff9d00")
+    (cons 220 "#ff6804")
+    (cons 240 "#ff3408")
+    (cons 260 "#ff000d")
+    (cons 280 "#d01226")
+    (cons 300 "#a12540")
+    (cons 320 "#723759")
+    (cons 340 "#444a73")
+    (cons 360 "#444a73")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,107 +1,57 @@
-;; Remove shit
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+;;; init.el -*- lexical-binding: t; -*-
+;;
+;; Author:  Henrik Lissner <contact@henrik.io>
+;; URL:     https://github.com/hlissner/doom-emacs
+;;
+;;   =================     ===============     ===============   ========  ========
+;;   \\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
+;;   ||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
+;;   || . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
+;;   ||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
+;;   || . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
+;;   ||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
+;;   || . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
+;;   ||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
+;;   ||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
+;;   ||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
+;;   ||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
+;;   ||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
+;;   ||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
+;;   ||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
+;;   ||.=='    _-'                                                     `' |  /==.||
+;;   =='    _-'                                                            \/   `==
+;;   \   _-'                                                                `-_   /
+;;    `''                                                                      ``'
+;;
+;; These demons are not part of GNU Emacs.
+;;
+;;; License: MIT
 
-;; melpa
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;; In the strange case that early-init.el wasn't loaded (e.g. you're using
+;; Chemacs 1? Or you're loading this file directly?), we do it explicitly:
+(unless (boundp 'doom-version)
+  (load (concat (file-name-directory load-file-name) "early-init")
+        nil t))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Ensure Doom's core libraries are properly initialized, autoloads file is
+;; loaded, and hooks set up for an interactive session.
+(doom-initialize)
 
-(eval-when-compile
-  (require 'use-package)
-  (setq use-package-verbose t))
-
-(use-package flatland-black-theme
-  :ensure t
-  :init
-  (progn
-    (load-theme 'flatland-black :no-confirm)
-    (setf frame-background-mode 'dark)
-    (global-hl-line-mode 1)))
-
-(use-package key-chord
-  :ensure t
-  :config
-  (key-chord-mode 1))
-
-;; load evil
-(use-package evil
-  :ensure t ;; install the evil package if not installed
-  :init ;; tweak evil's configuration before loading it
-  (setq evil-search-module 'evil-search)
-  (setq evil-ex-complete-emacs-commands nil)
-  (setq evil-vsplit-window-right t)
-  (setq evil-split-window-below t)
-  (setq evil-shift-round nil)
-  (setq evil-want-C-u-scroll t)
-  :config
-  (evil-mode)
-
-  ;; example how to map a command in normal mode (called 'normal state' in evil)
-  (define-key evil-normal-state-map (kbd "SPC ff") 'counsel-find-file)
-  (define-key evil-insert-state-map (kbd "jk") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "kj") 'evil-normal-state))
-
-(use-package projectile
-  :ensure t
-  :init
-  (projectile-mode +1)
-  :config
-  (setq projectile-project-search-path '("~/dev/"))
-  (define-key evil-normal-state-map (kbd "SPC pd") 'projectile-discover-projects-in-search-path)
-  (define-key evil-normal-state-map (kbd "SPC pp") 'projectile-switch-project))
-
-;; Ivy and Counsel shit
-(use-package ivy
-  :ensure t
-  :defer 0.1
-  :diminish
-  :custom
-  (ivy-count-format "(%d/%d) ")
-  (ivy-use-virtual-buffers t)
-  :config
-  (ivy-mode)
-  (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
-  (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
-  (setq ivy-extra-directories nil))
-
-(use-package counsel
-  :ensure t
-  :after ivy
-  :config (counsel-mode))
-
-(use-package ivy-rich
-  :ensure t
-  :after ivy
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
-  :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
-
-(use-package swiper
-  :ensure t
-  :after ivy
-  :config
-  (define-key evil-normal-state-map (kbd "SPC /") 'swiper))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Now we load all enabled modules in the order dictated by your `doom!' block
+;; in $DOOMDIR/init.el. `doom-initialize-modules' loads them (and hooks) in the
+;; given order:
+;;
+;;   $DOOMDIR/init.el
+;;   {$DOOMDIR,~/.emacs.d}/modules/*/*/init.el
+;;   `doom-before-init-modules-hook'
+;;   {$DOOMDIR,~/.emacs.d}/modules/*/*/config.el
+;;   `doom-init-modules-hook'
+;;   $DOOMDIR/config.el
+;;   `doom-after-init-modules-hook'
+;;   `after-init-hook'
+;;   `emacs-startup-hook'
+;;   `doom-init-ui-hook'
+;;   `window-setup-hook'
+;;
+;; And then we're good to go!
+(doom-initialize-modules)
