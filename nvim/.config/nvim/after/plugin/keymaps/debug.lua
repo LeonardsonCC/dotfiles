d563a2a7a1a323d846872e88594e03546e33992a
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-07-29 18:23:56 -0300

    conflicts no more

--- nvim/.config/nvim/after/plugin/keymaps/debug.lua
+++ nvim/.config/nvim/after/plugin/keymaps/debug.lua
@@ -1,13 +1,13 @@
 local nnoremap = require('my.keymap').nnoremap
 
 nnoremap('<Leader>dc', require('dap').continue)
-nnoremap('<Leader>dso', require('dap').step_over)
-nnoremap('<Leader>dsi', require('dap').step_into)
-nnoremap('<Leader>dst', require('dap').step_out)
+nnoremap('<Leader>do', require('dap').step_over)
+nnoremap('<Leader>di', require('dap').step_into)
+nnoremap('<Leader>dO', require('dap').step_out)
 nnoremap('<Leader>db', require('dap').toggle_breakpoint)
 nnoremap('<Leader>dB', function()
   require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
 end)
 nnoremap('<Leader>dr', require('dap').repl.open)
 
-nnoremap('<Leader>di', require('dapui').toggle)
+nnoremap('<Leader>du', require('dapui').toggle)
--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,105 +1,105 @@
-local Remap = require 'my.keymap'
-local nnoremap = Remap.nnoremap
-local inoremap = Remap.inoremap
+  local Remap = require 'my.keymap'
+  local nnoremap = Remap.nnoremap
+  local inoremap = Remap.inoremap
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local cmp = require 'cmp'
-local source_mapping = {
-  buffer = '[Buffer]',
-  nvim_lsp = '[LSP]',
-  nvim_lua = '[Lua]',
-  path = '[Path]',
-  nvim_lsp_signature_help = '[LSP_S]',
-  luasnip = '[Snippet]',
-  cmdline = '[CMD]',
-}
+  local cmp = require 'cmp'
+  local source_mapping = {
+    buffer = '[Buffer]',
+    nvim_lsp = '[LSP]',
+    nvim_lua = '[Lua]',
+    path = '[Path]',
+    nvim_lsp_signature_help = '[LSP_S]',
+    luasnip = '[Snippet]',
+    cmdline = '[CMD]',
+  }
 
-local lspkind = require 'lspkind'
+  local lspkind = require 'lspkind'
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping {
-      i = cmp.mapping.abort(),
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
     },
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
+    mapping = {
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-e>'] = cmp.mapping {
+        i = cmp.mapping.abort(),
+      },
+      ['<CR>'] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      ['<C-n>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+        else
+          fallback()
+        end
+      end,
+      ['<C-p>'] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
+        else
+          fallback()
+        end
+      end,
     },
-    ['<C-n>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-      else
-        fallback()
-      end
-    end,
-    ['<C-p>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item { behavior = cmp.SelectBehavior.Select }
-      else
-        fallback()
-      end
-    end,
-  },
-  window = {
-    completion = {
-      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
-      col_offset = 0,
-      side_padding = 0,
+    window = {
+      completion = {
+        winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+        col_offset = 0,
+        side_padding = 0,
+      },
     },
-  },
-  formatting = {
-    fields = { 'kind', 'abbr', 'menu' },
-    format = function(entry, vim_item)
-      local kind = lspkind.cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
-      local strings = vim.split(kind.kind, '%s', { trimempty = true })
-      kind.kind = ' ' .. strings[1] .. ' '
-      kind.menu = '    ' .. source_mapping[entry.source.name] .. ''
+    formatting = {
+      fields = { 'kind', 'abbr', 'menu' },
+      format = function(entry, vim_item)
+        local kind = lspkind.cmp_format { mode = 'symbol_text', maxwidth = 50 } (entry, vim_item)
+        local strings = vim.split(kind.kind, '%s', { trimempty = true })
+        kind.kind = ' ' .. strings[1] .. ' '
+        kind.menu = '    ' .. source_mapping[entry.source.name] .. ''
 
-      return kind
-    end,
-  },
-  sources = {
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'nvim_lua' },
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'buffer', keyword_length = 5 },
-  },
-  experimental = {
-    ghost_text = false,
-  },
-}
+        return kind
+      end,
+    },
+    sources = {
+      { name = 'nvim_lsp_signature_help' },
+      { name = 'nvim_lua' },
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'path' },
+      { name = 'buffer', keyword_length = 5 },
+    },
+    experimental = {
+      ghost_text = false,
+    },
+  }
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'cmdline' },
-  },
-})
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'cmdline' },
+    },
+  })
 
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp_document_symbol' },
-  }, {
-    { name = 'buffer' },
-  }),
-})
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp_document_symbol' },
+    }, {
+      { name = 'buffer' },
+    }),
+  })
 
-cmp.setup.filetype({ 'dap-repl', 'dapui_watches' }, {
-  sources = {
-    { name = 'dap' },
-  },
-})
+  cmp.setup.filetype({ 'dap-repl', 'dapui_watches' }, {
+    sources = {
+      { name = 'dap' },
+    },
+  })
 
 -- LSP setup
 local function config(_config)
