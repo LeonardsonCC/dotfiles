90c75c97756ed892136aa1623b62688d08ea70e9
LeonardsonCC <leonardson.carvalho@gmail.com>
2022-10-05 22:19:40 -0300

    nvim: keybingins....

--- nvim/.config/nvim/after/plugin/keymaps/init.lua
+++ nvim/.config/nvim/after/plugin/keymaps/init.lua
@@ -6,11 +6,27 @@ inoremap('jk', '<Esc>')
 inoremap('kj', '<Esc>')
 
 -- Nvim Tree
-nnoremap('<Leader>pv', '<Cmd>NvimTreeToggle<CR>')
-nnoremap('<Leader>pc', '<Cmd>NvimTreeFindFile<CR>')
+nnoremap('<Leader>pv', '<Cmd>NvimTreeFindFileToggle<CR>')
 
 -- source files
 nnoremap('<Leader>sc', '<Cmd>so %<CR>')
 
 -- undotree
 nnoremap('<Leader>ut', '<Cmd>UndotreeToggle<CR>')
+
+-- Packer
+nnoremap('<Leader>ps', '<Cmd>PackerSync<CR>')
+
+-- Term
+nnoremap('<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+
+-- Git
+local gs = require('gitsigns')
+nnoremap('<Leader>gs', '<CMD>Gitsigns stage_hunk<CR>')
+nnoremap('<Leader>gr', '<CMD>Gitsigns reset_hunk<CR>')
+nnoremap('<Leader>gu', gs.undo_stage_hunk)
+nnoremap('<Leader>gp', gs.preview_hunk)
+nnoremap('<Leader>gl', gs.toggle_current_line_blame)
+nnoremap('<Leader>gt', gs.diffthis)
+
--- nvim/.config/nvim/lua/my/term.lua
+++ nvim/.config/nvim/lua/my/term.lua
@@ -6,6 +6,3 @@ require 'FTerm'.setup({
   },
 })
 
--- Example keybindings
-vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
