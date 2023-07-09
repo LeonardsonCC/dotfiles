3c2ae362f1663adae9760bfdc3583cc726339787
LeonardsonCC <leonardson.carvalho@gmail.com>
2023-07-09 10:09:03 -0300

    fix wpp and added notify to change brightness

--- sway/.local/bin/sway-bg.sh
+++ sway/.local/bin/sway-bg.sh
@@ -4,6 +4,6 @@ TIME_TO_CHANGE_WALLPAPER=3600
 
 while :
 do
-  swaymsg "output * background $(find $HOME/wallpapers/ -type f | shuf -n 1) fill"
+  swaymsg "output * background $(find $HOME/wallpapers/ -type f -not -path '*/\.git/*' | shuf -n 1) fill"
   sleep $TIME_TO_CHANGE_WALLPAPER
 done
