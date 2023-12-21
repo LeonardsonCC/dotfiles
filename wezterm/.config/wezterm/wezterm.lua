68ce040c1891369d24fbb2c6befd58b3626c368d
LeonardsonCC <leonardson.carvalho@gmail.com>
2023-12-20 22:58:26 -0300

    updates :)

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ config.color_scheme = "IR_Black"
 
 config.font = wezterm.font("Noto Mono Nerd Font")
 config.font_size = 11
+config.line_height = 1.3
 
 config.use_fancy_tab_bar = true
 config.window_decorations = "INTEGRATED_BUTTONS"
