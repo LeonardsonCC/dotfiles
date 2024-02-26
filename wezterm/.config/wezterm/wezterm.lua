212c7cf1f5b0f44e543bf5ceb79ae270acd57a7e
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-02-26 09:56:22 -0300

    better configs to wez

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require("wezterm")
-local config = {}
+local config = wezterm.config_builder()
 
 -- config.color_scheme = "IR_Black"
 config.color_scheme = "tokyonight_night"
@@ -7,14 +7,22 @@ config.colors = {
 	background = "black",
 }
 
-config.font = wezterm.font("MonaspiceNe Nerd Font Mono")
-config.font_size = 10
-config.line_height = 1.3
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"nonicons",
+})
 
-config.enable_tab_bar = false
+config.font_size = 10
+config.line_height = 1.1
+
+-- config.enable_tab_bar = false
 config.window_decorations = "INTEGRATED_BUTTONS"
 config.integrated_title_button_style = "Gnome"
 
 config.window_background_opacity = 0.96
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.freetype_load_target = "HorizontalLcd"
 
 return config
