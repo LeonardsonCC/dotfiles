530c2a7e1b9c604bff8f6720cad3012baa5ac7e6
LeonardsonCC <leonardson.carvalho@gmail.com>
2024-05-20 08:23:20 -0300

    long time no commit

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,27 @@
 local wezterm = require("wezterm")
 local config = {}
 
--- config.color_scheme = "IR_Black"
 config.color_scheme = "tokyonight_night"
 config.colors = {
 	background = "black",
 }
 
+config.freetype_load_target = "HorizontalLcd"
 config.font = wezterm.font_with_fallback({
-	"Geist Mono",
 	"JetBrains Mono",
 	"nonicons",
 })
 
 config.font_size = 10
-config.line_height = 1.1
+config.line_height = 1.0
 
--- config.enable_tab_bar = false
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.integrated_title_button_style = "Gnome"
 
-config.window_background_opacity = 0.96
+config.window_background_opacity = 1
 
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.freetype_load_target = "HorizontalLcd"
 config.enable_kitty_graphics = true
 
 -- config.front_end = "OpenGL"
