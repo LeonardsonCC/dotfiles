798afebf2a3a2f7a4749b66fc4a419e6fea18338
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-07 10:46:34 -0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -728,6 +728,7 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				go = { "goimports-reviser" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
