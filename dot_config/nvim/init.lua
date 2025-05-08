3259597b023f3421d8a0e77d4369968fb6039f19
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-07 21:28:45 -0300

    nice

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -696,48 +696,6 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- Autoformat
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				"<leader>cf",
-				function()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
-				end
-			end,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				go = { "goimports-reviser" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
-			},
-		},
-	},
-
 	{ -- Autocompletion
 		"saghen/blink.cmp",
 		event = "VimEnter",
@@ -955,7 +913,7 @@ require("lazy").setup({
 	require("kickstart.plugins.autopairs"),
 	require("kickstart.plugins.neo-tree"),
 	require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
-	{ import = "custom.plugins" },
+	{ import = "core.plugins" },
 }, {
 	ui = {
 		icons = vim.g.have_nerd_font and {} or {
---
+++ dot_config/nvim/lua/core/plugins/conform.lua
@@ -0,0 +1,41 @@
+local config = {
+	-- disable autoformat for ft
+	disable_filetypes = { c = true, cpp = true },
+
+	-- select formatters for ft
+	formatters_by_ft = {
+		lua = { "stylua" },
+		go = { "goimports-reviser" },
+	},
+}
+
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[C]ode [F]ormat",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			local disable_filetypes = config.disable_filetypes
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				return nil
+			else
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end
+		end,
+		formatters_by_ft = config.formatters_by_ft,
+	},
+}
