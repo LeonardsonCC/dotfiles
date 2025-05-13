666e48c935155d6178bc949721d8507c0a81de63
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-12 23:58:29 -0300

    Update .config/waybar/custom/media_player.py

--- dot_config/waybar/custom/media_player.py
+++ dot_config/waybar/custom/media_player.py
@@ -124,7 +124,7 @@ class PlayerManager:
         elif artist is not None and title is not None:
             if player.props.metadata['mpris:artUrl'] is not None:
                 art_url = player.props.metadata['mpris:artUrl']
-                track_info = f"<img style=\"border-radius: 90%\" src=\"{art_url}\"> {artist} - {title}"
+                track_info = f"<div style=\"border-radius: 90%; background-image: url('{art_url}'; width: 50px height: 50px)\"></div> {artist} - {title}"
             else:
                 track_info = f"{artist} - {title}"
         else:
