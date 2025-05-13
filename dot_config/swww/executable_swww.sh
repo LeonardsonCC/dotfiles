9607e0f637bc37535cbd50bb56276f2f144f47e8
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-13 19:19:06 -0300

    Update .config/swww/swww.sh

--- dot_config/swww/executable_swww.sh
+++ dot_config/swww/executable_swww.sh
@@ -4,4 +4,4 @@ WALLPAPERS_DIR=/home/leonardson/repos/wallpapers/
 
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f -not -path "*.git*" | shuf -n 1)
 
-swww img "$WALLPAPER"
+/home/leonardson/.local/bin/swww img "$WALLPAPER"
