e661f2472fa906a7f878f6277a714fea761cba27
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-16 18:36:56 -0300

    Update .config/swww/swww.sh

--- dot_config/swww/executable_swww.sh
+++ dot_config/swww/executable_swww.sh
@@ -5,6 +5,7 @@ WALLPAPERS_DIR=/home/leonardson/repos/wallpapers/
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f -not -path "*.git*" | shuf -n 1)
 
 while ! swww query &> /dev/null; do
+    echo "waiting..."
     sleep 1
 done
 
