baf1bae0ca62c8bc68f61492cfb26d111e16acdf
LeonardsonCC <leonardson.carvalho@gmail.com>
2025-05-17 01:02:46 -0300

    Update .config/swww/swww.sh Update .config/systemd/user/swww.service

--- dot_config/swww/executable_swww.sh
+++ dot_config/swww/executable_swww.sh
@@ -4,9 +4,9 @@ WALLPAPERS_DIR=/home/leonardson/repos/wallpapers/
 
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f -not -path "*.git*" | shuf -n 1)
 
-while ! /home/leonardson/.local/bin/swww query &> /dev/null; do
+while ! swww query &> /dev/null; do
     echo "waiting..."
     sleep 1
 done
 
-/home/leonardson/.local/bin/swww img "$WALLPAPER" --transition-type center
+swww img "$WALLPAPER" --transition-type center
