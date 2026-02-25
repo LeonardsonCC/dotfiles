c8b2a0268d0037de58305918d8c4b49ce960e690
LeonardsonCC <leonardson.carvalho@gmail.com>
2026-02-25 23:52:36 +0000

    test

--- dot_config/nvim/lua/core/plugins/split_join.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"Wansmer/treesj",
-	keys = { "<space>m" },
-	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
-	config = function()
-		require("treesj").setup({})
-	end,
-}
